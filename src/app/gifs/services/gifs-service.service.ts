24a5bf0e0565e0ff94a66397f181232d95dcfeed
jaime <pepefloress.1999@gmail.com>
2023-12-06 13:58:06 -0600

    se cambian los parametros

--- src/app/gifs/services/gifs-service.service.ts
+++ src/app/gifs/services/gifs-service.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -33,18 +33,24 @@ export class GifsService {
   }
 
 
-  public searchTag(tag:string){
+  public searchTag(tag:string):void{
 
     if(tag.length===0) return;
     this.organizeHistory(tag);
 
+    const params = new HttpParams()
+    .set('api_key',this.apiKey)
+    .set('limit','10')
+    .set('1',tag)
 
 
-    this.httpClient.get('api.giphy.com/v1/gifs/search?api_key=dw9B0vmpvd91JCNR9V4Gf0ip71gToThD&q=pokemon&limit=10')
+
+    this.httpClient.get(`${this.apiKey}/search`, {params})
     .subscribe(resp=>{
       console.log(resp)
 
     })
+
 
 
 
