e2d30b1df9d2fa36024936d8a4da88108227b100
jaime <pepefloress.1999@gmail.com>
2023-12-05 23:46:15 -0600

    hola

--- src/app/gifs/services/gifs-service.service.ts
+++ src/app/gifs/services/gifs-service.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -6,12 +7,15 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory:string[] = [];
+  private apiKey:string = 'dw9B0vmpvd91JCNR9V4Gf0ip71gToThD';
 
-  constructor() { }
+
+  constructor(private httpClient:HttpClient) { }
 
   get tagsHistory(){
     return [...this._tagsHistory];
   }
+
 
 
   organizeHistory(tag:string){
@@ -31,8 +35,16 @@ export class GifsService {
   public searchTag(tag:string){
 
     if(tag.length===0) return;
-
     this.organizeHistory(tag);
+
+    
+
+    this.httpClient.get('api.giphy.com/v1/gifs/search?api_key=dw9B0vmpvd91JCNR9V4Gf0ip71gToThD&q=pokemon&limit=10')
+    .subscribe(resp=>{
+      console.log(resp)
+      // commit 
+    })
+    
 
 
 
