c94fcaa7dcfdd961d54dd3bc4a34bae433df1e2b
jaime <pepefloress.1999@gmail.com>
2023-12-07 16:02:09 -0600

    ejemplo

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,32 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../../services/gifs-service.service';
+
+@Component({
+  selector: 'app-search-box',
+  templateUrl: './search-box.component.html',
+  styleUrls: ['./search-box.component.css']
+})
+export class SearchBoxComponent {
+
+  constructor(private gifsService:GifsService){}
+
+
+
+  @ViewChild('txtTagInput')
+  public tagInput!: ElementRef<HTMLInputElement>;
+
+
+
+  searchTag()  {
+
+      const newTag = this.tagInput.nativeElement.value
+      console.log(newTag)
+
+      this.gifsService.searchTag(newTag)
+
+      this.tagInput.nativeElement.value='';
+
+
+  }
+
+}
---
+++ src/app/gifs/services/gifs-service.service.ts
@@ -0,0 +1,64 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GifsService {
+
+  public gifList = [];
+
+  private _tagsHistory:string[] = [];
+  private apiKey:string = 'dw9B0vmpvd91JCNR9V4Gf0ip71gToThD';
+
+
+  constructor(private httpClient:HttpClient) { }
+
+  get tagsHistory(){
+    return [...this._tagsHistory];
+  }
+
+
+
+  organizeHistory(tag:string){
+    tag = tag.toLocaleLowerCase(); // esto es para volver a minusculas
+
+    if(this._tagsHistory.includes(tag)){
+      this._tagsHistory = this._tagsHistory.filter((oldTag)=>oldTag!==tag) // es para filtrar solo los tags que son diferentes al tag
+    }
+
+    this._tagsHistory.unshift(tag); // es para agregar el tag al incio del arrelgo
+
+    this._tagsHistory = this._tagsHistory.splice(0,10) // es para solo alla 10 propiedades en el arreglo
+
+  }
+
+
+  public searchTag(tag:string):void{
+
+    if(tag.length===0) return;
+    this.organizeHistory(tag);
+
+    const params = new HttpParams()
+    .set('api_key',this.apiKey)
+    .set('limit','10')
+    .set('1',tag)
+
+
+
+    this.httpClient.get<any>(`${this.apiKey}/search`, {params})
+    .subscribe(resp=>{
+
+      this.gifList = resp.data
+      console.log(resp)
+      //este es un ejemplo 
+
+    })
+
+
+
+
+
+  }
+}
+
