06f8fb4c5adcc1d02169507c25cea30dce02f60d
jaime <pepefloress.1999@gmail.com>
2023-12-07 16:22:49 -0600

    se agrega input

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-list',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-list.component.css']
 })
 export class CardListComponent {
+
+  constructor (){}
+
+  @Input()
+  public gifs:any
 
 }
--- src/app/gifs/pages/home/home.component.ts
+++ src/app/gifs/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GifsService } from '../../services/gifs-service.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+
+  constructor (private gifsService:GifsService) {}
+
+  get gifs(){
+    return this.gifsService.gifList;
+  }
+
 
 }
